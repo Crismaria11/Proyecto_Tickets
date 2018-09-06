6154c88b1b70ab13dc676fc5ce8738430fa6f148
Crismaria11 <38018701+Crismaria11@users.noreply.github.com>
2018-09-06 09:25:31 -0600

    Add files via upload

---
+++ Cine.java
@@ -0,0 +1,34 @@
+import java.io.*;
+public class Cine {
+    /* Ejemplo de como irian los asientos en el cine por el momento.
+     * | [A1] [B1] [C1] [D1] [E1] |
+     * | [A2] [B2] [C2] [D2] [E2] |
+     * | [A3] [B3] [C3] [D3] [E3] |
+     */
+    public static void main(String[] args) {
+        InputStreamReader inStream = new InputStreamReader(System.in);
+        BufferedReader bufRead = new BufferedReader(inStream);
+        compra1 A1 = new compra1(null, null, null, null);
+
+        compra1 A2 = new compra1(null, null, null, null);
+
+        compra1 A3 = new compra1(null, null, null, null)
+
+        compra1 B1 = new compra1(null, null, null, null);
+
+        compra1 B2 = new compra1(null, null, null, null);
+
+        compra1 C1 = new compra1(null, null, null, null);
+
+        compra1 C2 = new compra1(null, null, null, null);
+
+        compra1 D1 = new compra1(null, null, null, null);
+
+        compra1 D2 = new compra1(null, null, null, null);
+
+        compra1 E1 = new compra1(null, null, null, null);
+
+        compra1 E2 = new compra1(null, null, null, null);
+    }
+
+}

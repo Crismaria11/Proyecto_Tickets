c4e81463ea0fad665bc00ed2ce167e27216f977d
Crismaria11 <38018701+Crismaria11@users.noreply.github.com>
2018-09-06 09:00:35 -0600

    Add files via upload

--- compra1.java
+++ compra1.java
@@ -1,7 +1,22 @@
 public class compra1 {
     String firstName;
     String lastName;
-    String address;
-    String phoneNumber;
-}
+    String correo;
+    String contrasenia;
 
+
+    // Constructor
+    public void set(String firstName, String lastName, String correo, String contrasenia) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+
+    }
+
+    public compra1(String f, String l, String cor, String con) {
+        this.set(f,l,cor,con);
+
+
+    }
+}
